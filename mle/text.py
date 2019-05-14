cdc70afd75e6100da4733d8c02f41a322dfa4fae
Max Leander <mlea@hellofresh.com>
2019-05-14 14:56:26 +0200

    add docstring

--- mle/text.py
+++ mle/text.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import cross_val_score
 from sklearn.pipeline import Pipeline
 
 
-def tfidf_logreg(train_df: pd.DataFrame, test_df: pd.DataFrame, text_col: str, target_col: str):
+def predict_numerical_from_text(train_df: pd.DataFrame, test_df: pd.DataFrame, text_col: str, target_col: str):
     """
     Predict a numerical value between 0 and 1 from text using Logistic Regression on TF-IDF features.
     Specifically, the model will predict the probability of the target being below or above 0.5.
@@ -17,7 +17,8 @@ def tfidf_logreg(train_df: pd.DataFrame, test_df: pd.DataFrame, text_col: str, t
     :param test_df: a Pandas dataframe containing the test_df set
     :param text_col: the column containing the text that will be used as features
     :param target_col: the column containing the numerical target
-    :return: a tuple containing the test_df set predictions as well as the mean cross-validation score
+
+    :return: a tuple containing the test set predictions as well as the mean cross-validation score
     """
     train_df['class'] = train_df[target_col] >= 0.5
 
@@ -37,8 +38,19 @@ def tfidf_logreg(train_df: pd.DataFrame, test_df: pd.DataFrame, text_col: str, t
     return predicted, mean_clf_score
 
 
-def tfidf_multiclass(train_df: pd.DataFrame, test_df: pd.DataFrame, text_col: str, target_col: str,
-                     metric='neg_log_loss'):
+def predict_multiclass_from_text(train_df: pd.DataFrame, test_df: pd.DataFrame, text_col: str, target_col: str,
+                                 metric='neg_log_loss'):
+    """
+        Multi-class classification from text using Logistic Regression on TF-IDF features.
+
+        :param train_df: a Pandas dataframe containing the training set
+        :param test_df: a Pandas dataframe containing the test_df set
+        :param text_col: the column containing the text that will be used as features
+        :param target_col: the column containing the numerical target
+        :param metric: scoring metric for cross-validation
+
+        :return: a tuple containing the test set predictions as well as the mean cross-validation score
+        """
     text_clf = Pipeline([
         ('vect', CountVectorizer()),
         ('tfidf', TfidfTransformer()),
